41c1e98ef8558285f7fa2b45a27a0632a9c2dc2e
Matt Marshall <matthew.mrshll@gmail.com>
2015-06-19 10:44:44 +0100

    Skeleton of the OrgModel completed. Needs graph functionality but atm want to get it working with the template

--- site/src/Mrshll/SiteBundle/Model/OrgModel.php
+++ site/src/Mrshll/SiteBundle/Model/OrgModel.php
@@ -14,6 +14,7 @@ class OrgModel {
   private $name;
   private $description;
   private $website;
+  private $totalEmployees;
 
   //======================================================
   // Basic Getter and setter stuff here
@@ -44,19 +45,41 @@ class OrgModel {
     return $this->description;
   }
 
+  public function getWebsite()
+  {
+    return $this->website;
+  }
+
+
   public function setWebsite($website)
   {
     $this->website = $website;
   }
 
+  public function getTotalEmployees()
+  {
+    return $this->totalEmployees;
+  }
+
   // ====================================================
-  // Functions to interact with the graph here
+  // Functions to interact with the graph here -- rememeber you should be returning arrays of nodes and edges from this node to the others ;-)
   // ====================================================
+
+  public function addToGraph()
+  {
+    // Code to add project to the graph here. rememeber to do the null check on the id, only upload to graph if null as otherwise you'll be creating a new node.
+  }
+
+  public function addProject($project)
+  {
+    // Code to add a project here
+  }
 
   public function getAllProjects()
   {
-    return "This Organisation has no projects listed";
+    return 0;
   }
+
 
   public function getActiveProjects()
   {
