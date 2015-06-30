a669e3fddd34ad9dd7f8c4d89c9ab6718dc8ab66
Matt Marshall <matthew.mrshll@gmail.com>
2015-06-30 09:39:00 +0100

    Began CSV Parser as the Northumbria one was too complicated to fix

---
+++ site/src/Mrshll/SiteBundle/Helper/CSVParser.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Class to assist the parsing of CSV files into Record Objects
+ */
+
+ namespace Mrshll\SiteBundle\Helper;
+
+ class CSVParser
+ {
+
+   /**
+    * Parses all the Northumbria CSVs
+    *
+    */
+    public function parseNorthumbria()
+    {
+      
+    }
+ }
